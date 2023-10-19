1722ea586017a9da5152930d4c9d0d0c90e5f5a8
Zafar Khan <Z.Khan@tees.ac.uk>
2023-10-19 03:39:58 +0100

    Add project files.

---
+++ Vista.Api/Program.cs
@@ -0,0 +1,30 @@
+using Vista.Api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register database context with the framework - this needs to be added manually
+builder.Services.AddDbContext<TrainersDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
